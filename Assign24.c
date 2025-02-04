7392998fd0cd66ac00a619d6ec122a9e39fa3b90
unknown <mritunjayku99@gmail.com>
2025-02-04 13:36:31 +0530

    Assign24.c is modifiedand Assign25.c is added

--- Assign24.c
+++ Assign24.c
@@ -2,6 +2,8 @@
 void printPrimeNumbers(int,int);
 void printFirstNTermsFibonacciSeries(int);
 void printArmstrongNumbers(int,int);
+void Sum_Of_First_N_Terms(int);
+int factorial(int);
 /*Q1.Write a function to print all prime numbers between two given numbers(TSRN)*/
 void printPrimeNumbers(int num1,int num2)
 {
@@ -32,6 +34,26 @@ void printFirstNTermsFibonacciSeries(int num)
     }
 }
 
+/*Q3. Write a function to find the sum of first N terms of series 1!/1+2!/2+3!/3+4!/4+5!/5+....+n!/n(TSRN) */
+int factorial(int num)
+{
+    int f=1;
+    while(num)
+    {
+        f=f*num;
+        num--;
+    }
+    return f;
+}
+void Sum_Of_First_N_Terms(int n)
+{
+   int sum,i;
+   for(i=1,sum=0;i<=n;i++)
+   {
+     sum=sum+(factorial(i))/i;
+   }
+   printf("Sum of first %d series=%d",n,sum);
+}
 /*Q4.Write a function to print all Armstrong numbers between two given numbers(TSRN)*/
 void printArmstrongNumbers(int num1,int num2)
 {
@@ -64,4 +86,12 @@ void printArmstrongNumbers(int num1,int num2)
          printf("%d ",num1);
     }
 }
-
+/*Q5. Write a function to print PASCAL Triangle*/
+int main()
+{
+    int num;
+    printf("Enter a number ");
+    scanf("%d",&num);
+    Sum_Of_First_N_Terms(num);
+    return 0;
+}
