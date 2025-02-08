c9f3ebe95e19e88150fe0936ffef53b4090f2221
unknown <mritunjayku99@gmail.com>
2025-02-08 21:00:12 +0530

    Assign26.c and Assign28.c files are modified

--- Assign26.c
+++ Assign26.c
@@ -1,52 +1,65 @@
 #include<stdio.h>
 void Print_First_N_Even_Natural_Numbers_Reverse_Order(int);
 void Print_Squares_Of_First_N_Natural_Numbers(int);
-int Binary_Of_Decimal_Number(int);
-int Octal_Of_Decimal_Number(int);
-int Reverse_Of_Number(int,int);
+void Binary_Of_Decimal_Number(int);
+void Octal_Of_Decimal_Number(int);
+void Reverse_Of_Number(int);
 /*Q1. Write a recursive function to print first N even natural numbers in reverse order*/
 void Print_First_N_Even_Natural_Numbers_Reverse_Order(int num)
 {
-    if(num==0)
-     return;
-    printf("%d ",2*num);
-    Print_First_N_Even_Natural_Numbers_Reverse_Order(num-1);
+    if(num>0)
+    {
+        printf("%d ",2*num);
+        Print_First_N_Even_Natural_Numbers_Reverse_Order(num-1);
+    }
 }
 /*Q2. Write a recursive function to print squares of first N natural numbers*/
 void Print_Squares_Of_First_N_Natural_Numbers(int num)
 {
-    if(num==0)
-     return;
-    Print_Squares_Of_First_N_Natural_Numbers(num-1);
-    printf("%d ",num*num);
+    if(num>0)
+    {
+        Print_Squares_Of_First_N_Natural_Numbers(num-1);
+        printf("%d ",num*num);
+    }
 }
 /*Q3. Write a recursive function to print binary of a given decimal number*/
-int Binary_Of_Decimal_Number(int num)
+void Binary_Of_Decimal_Number(int num)
 {
-    if(num==0)
-     return 0;
-    return Binary_Of_Decimal_Number(num/2)*10+num%2;
+    if(num)
+    {
+        Binary_Of_Decimal_Number(num/2);
+        printf("%d",num%2);
+    }
 }
 /*Q4. Write a recursive function to print octal of a given decimal number*/
-int Octal_Of_Decimal_Number(int num)
+void Octal_Of_Decimal_Number(int num)
 {
-    if(num==0)
-     return 0;
-    return Octal_Of_Decimal_Number(num/8)*10+num%8;
+    if(num)
+    {
+      Octal_Of_Decimal_Number(num/8);
+      printf("%d",num%8);
+    }
 }
 /*Q5. Write a recursive function to print reverse of a given number*/
-int Reverse_Of_Number(int num,int rev)
+void Reverse_Of_Number(int num)
 {
-    if(num==0)
-     return rev;
-    return Reverse_Of_Number(num/10,rev*10+num%10);
+    if(num)
+    {
+        printf("%d",num%10);
+        Reverse_Of_Number(num/10);
+    }
 }
 int main()
 {
-  int num,res,rev=0;
-  printf("Enter a number ");
-  scanf("%d",&num);
-  res=Reverse_Of_Number(num,rev);
-  printf("Reverse of %d is %d",num,res);
-  return 0;
+//   int num,res,rev=0;
+//   printf("Enter a number ");
+//   scanf("%d",&num);
+//   res=Reverse_Of_Number(num,rev);
+//   printf("Reverse of %d is %d",num,res);
+int n;
+printf("Enter a number ");
+scanf("%d",&n);
+printf("Reverse of %d is ",n);
+Reverse_Of_Number(n);
+return 0;
 }
--- Assign28.c
+++ Assign28.c
@@ -1,30 +1,35 @@
 #include<stdio.h>
 int factorial(int);
-int HCF_Of_Two_Numbers(int,int);
+int HCF_Of_Two_Numbers(int,int,int);
 void Print_First_N_Terms_Of_Fibonacci_Series(int,int,int,int);
 int Count_Digits(int);
 int Power_Of_Any_Number(int,int);
 /*Q1.Write a recursive function to calculate factorial of a given number*/
 int factorial(int num)
 {
-    if(num==1)
+    if(num==0)
      return 1;
     return num*factorial(num-1);
 }
 
 /*Q2.Write a recursive function to calculate HCF of two numbers*/
-int HCF_Of_Two_Numbers(int num1,int num2)
+int HCF_Of_Two_Numbers(int num1,int num2,int HCF)
 {
-
+    if(num1%HCF==0&&num2%HCF==0)
+    {
+        return HCF;
+    }
+    return HCF_Of_Two_Numbers(num1,num2,HCF-1);
 }
 
 /*Q3.Write a recursive function to print first N terms of Fibonacci series*/
 void Print_First_N_Terms_Of_Fibonacci_Series(int num,int a,int b,int c)
 {
-    if(num==0)
-     return;
-    printf("%d ",c);
-    Print_First_N_Terms_Of_Fibonacci_Series(num-1,b,c,b+c);
+    if(num)
+    {
+        printf("%d ",c);
+        Print_First_N_Terms_Of_Fibonacci_Series(num-1,b,c,b+c);
+    }
 }
 
 /*Q4.Write a program in c to count the digits of a given number using recursion */
@@ -52,10 +57,23 @@ int main()
      res=Power_Of_Any_Number(n,p);
      printf("%d to the power %d = %d",n,p,res);
     */
-    int n,res;
-    printf("Enter a number ");
-    scanf("%d",&n);
-    res=Count_Digits(n);
-    printf("Number of digits in %d is %d",n,res);
+    // int n,res;
+    // printf("Enter a number ");
+    // scanf("%d",&n);
+    // res=Count_Digits(n);
+    // printf("Number of digits in %d is %d",n,res);
+
+    // int n,a=-1,b=1,c=0;
+    // printf("Enter a number ");
+    // scanf("%d",&n);
+    // printf("First %d terms of Fibonacci Series\n",n);
+    // Print_First_N_Terms_Of_Fibonacci_Series(n,a,b,c);
+    
+    // int num1,num2,min,res;
+    // printf("Enter two numbers");
+    // scanf("%d%d",&num1,&num2);
+    // min=num1<num2?num1:num2;
+    // res=HCF_Of_Two_Numbers(num1,num2,min);
+    // printf("HCF of %d and %d is %d",num1,num2,res);
     return 0;
 }
