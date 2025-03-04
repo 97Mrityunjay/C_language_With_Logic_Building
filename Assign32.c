458f4f9ad5d643a770004a282b9970936d617f37
unknown <mritunjayku99@gmail.com>
2025-03-04 20:53:54 +0530

    Assign32.c file is modified

--- Assign32.c
+++ Assign32.c
@@ -1,4 +1,5 @@
-#include<Assign30.c>
+#include "Assign31.c"
+#include<stdio.h>
 int f1(int[],int,int,int);
 /*Q1.Write a function to swap two elements of given array with specified indices*/
 int f1(int arr[],int size,int index1,int index2)
@@ -12,24 +13,44 @@ int f1(int arr[],int size,int index1,int index2)
     }
 }
 /*Q2.Write a function to count a total number of duplicate elements in an array.(Means elements that occurs 2 times in an array)*/
-int countTotalNumberOfDuplicateElements(int arr[],int size)
+int countTotalNumberOfDuplicateElements(int a[],int size)
 {
-    int flag=0,count=0,n;
-    int i,j;
-    for(i=0;i<size-1;i++)
+    int b[size],i=0,j=0,k=0;
+    sortArray(a,size);
+    b[j]=a[i];
+    for(i=1,j=1;i<size;i++)
     {
-        for(j=i+1;j<size;j++)
+        if(a[i]>a[k])
         {
-            if(arr[i]==arr[j])
-            {
-                flag=1;
-            }
-           
-        }
-        if(flag==1)
-        {
-            count++;
-            n=arr[i];
+            b[j]=a[i];
+            j++;
+            k=i;
         }
     }
+    printf("Total number of duplicate elements=%d",size-j);
+}
+/*Q3.Write a function to print all unique elements in Array*/
+void printAllUniqueElements(int a[],int size)
+{
+    int b[size],i=0,j=0,k=0;
+    sortArray(a,size);
+    b[j]=a[i];
+    printf("%d ",b[j]);
+    for(i=1,j=1;i<size;i++)
+    {
+        if(a[i]>a[k])
+        {
+            b[j]=a[i];
+            printf("%d ",b[j]);
+            j++;
+            k=i;
+        }
+    }
+
+}
+int main()
+{
+    int arr[]={3,5,5,3,2,5,2,9,8,2,5,9};
+    printAllUniqueElements(arr,12);
+    return 0;
 }
