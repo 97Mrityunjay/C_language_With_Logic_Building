3a8e7e68bfc05f03f2e6f1aeb2baf9feac436ec2
unknown <mritunjayku99@gmail.com>
2025-03-04 20:54:56 +0530

    Assign31.c file is modified

--- Assign31.c
+++ Assign31.c
@@ -3,7 +3,7 @@ int Find_Greatest_Number(int[],int);
 int Find_Smallest_Number(int[],int);
 void sortArray(int[],int);
 void rotateArray(int[],int,int,char);
-int f1(int[],int);
+int f2(int[],int);
 /*Q1.Write a function to find the greatest number from the given array of any size(TSRS)*/
 int Find_Greatest_Number(int a[],int size)
 {
@@ -32,24 +32,33 @@ int Find_Smallest_Number(int a[],int size)
 /*Q3.Write a function to sort an array of any size(TSRN)*/
 void sortArray(int a[],int size)
 {
-    int i,j,temp;
-    for(i=0;i<size-1;i++)
+    int r,j,temp;
+    for(r=1;r<size;r++)
     {
-        for(j=i+1;j<size;j++)
+        // for(j=i+1;j<size;j++)
+        // {
+        //     if(a[j]<a[i])
+        //     {
+        //         temp=a[j];
+        //         a[j]=a[i];
+        //         a[i]=temp;
+        //     }
+        // }
+        for(j=0;j<size-r;j++)
         {
-            if(a[j]<a[i])
+            if(a[j]>a[j+1])
             {
                 temp=a[j];
-                a[j]=a[i];
-                a[i]=temp;
+                a[j]=a[j+1];
+                a[j+1]=temp;
             }
         }
     }
-    printf("After sorting Array elements are\n");
-    for(i=0;i<size;i++)
-    {
-        printf("%d ",a[i]);
-    }
+    // printf("After sorting Array elements are\n");
+    // for(j=0;j<size;j++)
+    // {
+    //     printf("%d ",a[j]);
+    // }
 }
 
 /*Q4.*/
@@ -88,7 +97,7 @@ void rotateArray(int a[],int size,int n,char d)
 }
 
 /*Q5. Write a function to find the first occurrence of adjacent duplicate values in the array.Function has to return the value of the element */
-int f1(int arr[],int size)
+int f2(int arr[],int size)
 {
     int i;
     for(i=0;i<size-1;i++)
@@ -98,9 +107,9 @@ int f1(int arr[],int size)
     }
     return -1;
 }
-int main()
+/*int main()
 {
-    /*int n,i;
+    int n,i;
     printf("Enter a number ");
     scanf("%d",&n);
     int arr[n];
@@ -109,17 +118,17 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
-    Sort(arr,n);
-    */
-    int size=10,arr[size],i,n;
-    char d='r';
-    printf("Enter %d numbers ",size);
-    for(i=0;i<size;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    printf("Enter position ");
-    scanf("%d",&n);
-    rotateArray(arr,size,n,d);
+    sortArray(arr,n);
+    
+    // int size=10,arr[size],i,n;
+    // char d='r';
+    // printf("Enter %d numbers ",size);
+    // for(i=0;i<size;i++)
+    // {
+    //     scanf("%d",&arr[i]);
+    // }
+    // printf("Enter position ");
+    // scanf("%d",&n);
+    // rotateArray(arr,size,n,d);
     return 0;
-}
+}*/
