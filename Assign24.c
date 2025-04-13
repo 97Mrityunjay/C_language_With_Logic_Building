bef41ca02b3048a7ecf7a2134b06c5dfdaadde42
unknown <mritunjayku99@gmail.com>
2025-04-14 00:35:34 +0530

    Assign24.c is modified

--- Assign24.c
+++ Assign24.c
@@ -1,22 +1,31 @@
 #include<stdio.h>
+#include "Assign23.c"
+#include "Assign22.c"
 void printPrimeNumbers(int,int);
 void printFirstNTermsFibonacciSeries(int);
 void printArmstrongNumbers(int,int);
 void Sum_Of_First_N_Terms(int);
-int factorial(int);
+void Pascal_Triangle(int);
+// int factorial(int);
 /*Q1.Write a function to print all prime numbers between two given numbers(TSRN)*/
+// int isPrime(int n)
+// {
+//     int divisor=2;
+//     while(divisor<n)
+//     {
+//         if(n%divisor==0)
+//          return 0;
+//         divisor++;
+//     }
+//     return 1;
+// }
 void printPrimeNumbers(int num1,int num2)
 {
-    int divisor;
-    for(num1=num1+1;num1<num2;num1++)
+    int divisor,num;
+    for(num=num1+1;num<num2;num++)
     {
-        for(divisor=2;divisor<num1;divisor++)
-        {
-            if(num1%divisor==0)
-             break;
-        }
-        if(divisor==num1)
-         printf("%d ",num1);
+        if(isPrime(num))
+         printf("%d ",num);
     }
 }
 
@@ -35,16 +44,16 @@ void printFirstNTermsFibonacciSeries(int num)
 }
 
 /*Q3. Write a function to find the sum of first N terms of series 1!/1+2!/2+3!/3+4!/4+5!/5+....+n!/n(TSRN) */
-int factorial(int num)
-{
-    int f=1;
-    while(num)
-    {
-        f=f*num;
-        num--;
-    }
-    return f;
-}
+// int factorial(int num)
+// {
+//     int f=1;
+//     while(num)
+//     {
+//         f=f*num;
+//         num--;
+//     }
+//     return f;
+// }
 void Sum_Of_First_N_Terms(int n)
 {
    int sum,i;
@@ -53,6 +62,27 @@ void Sum_Of_First_N_Terms(int n)
      sum=sum+(factorial(i))/i;
    }
    printf("Sum of first %d series=%d",n,sum);
+}
+/*3. Write a function to print PASCAL Triangle*/
+void Pascal_Triangle(int lines)
+{
+    int i,j,n,r;
+    for(i=1,n=0;i<=lines;i++,n++)
+    {
+        for(j=1,r=0;j<=2*lines-1;j++)
+        {
+            if(j>=lines+1-i && j<=lines-1+i && (i+j)%2)
+            {
+                
+                printf("%2d",numberOfCombinations(n,r++));
+            }
+            else 
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 }
 /*Q4.Write a function to print all Armstrong numbers between two given numbers(TSRN)*/
 void printArmstrongNumbers(int num1,int num2)
@@ -89,9 +119,10 @@ void printArmstrongNumbers(int num1,int num2)
 /*Q5. Write a function to print PASCAL Triangle*/
 int main()
 {
-    int num;
-    printf("Enter a number ");
-    scanf("%d",&num);
-    Sum_Of_First_N_Terms(num);
+    // int num;
+    // printf("Enter a number ");
+    // scanf("%d",&num);
+    // Sum_Of_First_N_Terms(num);
+    Pascal_Triangle(6);
     return 0;
 }
