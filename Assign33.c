74d564b75a8dab677b401cadfa33d9c3222c2df0
unknown <mritunjayku99@gmail.com>
2025-05-28 00:21:13 +0530

    Assign33.c file is added

---
+++ Assign33.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void sumOfTwoMatrices(int[][3],int[][3],int[][3],int);
+void sumOfTwoMatrices(int arr1[][3],int arr2[][3],int arr3[][3],int size)
+{
+    int i,j;
+    for(i=0;i<size;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            arr3[i][j]=arr1[i][j]+arr2[i][j];
+        }
+    }
+}
+int main()
+{
+    int arr1[3][3],arr2[3][3], arr3[3][3],r,c;
+    printf("Enter elements of first matrix\n");
+    for(r=0;r<3;r++)
+    {
+        for(c=0;c<3;c++)
+        {
+            scanf("%d",&arr1[r][c]);
+        }
+    }
+    printf("Enter second matrix\n");
+    for(r=0;r<3;r++)
+    {
+        for(c=0;c<3;c++)
+        {
+            scanf("%d",&arr2[r][c]);
+        }
+    }
+    sumOfTwoMatrices(arr1,arr2,arr3,3);
+    printf("Sum of two matrices\n");
+    for(r=0;r<3;r++)
+    {
+        for(c=0;c<3;c++)
+        {
+            printf("%d ",arr3[r][c]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
