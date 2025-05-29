c750ec3d900b59b8008cbc55f1e8128b097b091e
unknown <mritunjayku99@gmail.com>
2025-05-30 01:02:16 +0530

    code for matrix multiplication and transpose of matrix is added

--- Assign33.c
+++ Assign33.c
@@ -11,34 +11,103 @@ void sumOfTwoMatrices(int arr1[][3],int arr2[][3],int arr3[][3],int n)
         }
     }
 }
-int main()
+void f2()
 {
-    int arr1[3][3],arr2[3][3], arr3[3][3],r,c;
-    printf("Enter elements of first matrix\n");
-    for(r=0;r<3;r++)
+    int A[3][3],B[3][3],C[3][3],i,j,k,sum;
+    printf("Enter 9 elements of first matrix\n");
+    for(i=0;i<3;i++)
     {
-        for(c=0;c<3;c++)
+        for(j=0;j<3;j++)
         {
-            scanf("%d",&arr1[r][c]);
+            scanf("%d",&A[i][j]);
         }
     }
-    printf("Enter second matrix\n");
-    for(r=0;r<3;r++)
+    printf("Enter 9 elements of second matrix\n");
+    for(i=0;i<3;i++)
     {
-        for(c=0;c<3;c++)
+        for(j=0;j<3;j++)
         {
-            scanf("%d",&arr2[r][c]);
+            scanf("%d",&B[i][j]);
         }
     }
-    sumOfTwoMatrices(arr1,arr2,arr3,3);
-    printf("Sum of two matrices\n");
-    for(r=0;r<3;r++)
+    for(i=0;i<3;i++)
     {
-        for(c=0;c<3;c++)
+        for(j=0;j<3;j++)
         {
-            printf("%d ",arr3[r][c]);
+            sum=0;
+            for(k=0;k<3;k++)
+            {
+                sum+=A[i][k]*B[k][j];
+            }
+            C[i][j]=sum;
+        }
+    }
+    printf("After multiplication of two matrices, the resultant matrix\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",C[i][j]);
         }
         printf("\n");
     }
+}
+void f3()
+{
+    int A[2][3],B[3][2],i,j;
+    printf("Enter 6 elements of a matrix\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&A[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            B[i][j]=A[j][i];
+        }
+    }
+    printf("Transposed matrix\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<2;j++)
+         printf("%d ",B[i][j]);
+        printf("\n");
+    }
+
+}
+int main()
+{
+    // int arr1[3][3],arr2[3][3], arr3[3][3],r,c;
+    // printf("Enter elements of first matrix\n");
+    // for(r=0;r<3;r++)
+    // {
+    //     for(c=0;c<3;c++)
+    //     {
+    //         scanf("%d",&arr1[r][c]);
+    //     }
+    // }
+    // printf("Enter second matrix\n");
+    // for(r=0;r<3;r++)
+    // {
+    //     for(c=0;c<3;c++)
+    //     {
+    //         scanf("%d",&arr2[r][c]);
+    //     }
+    // }
+    // sumOfTwoMatrices(arr1,arr2,arr3,3);
+    // printf("Sum of two matrices\n");
+    // for(r=0;r<3;r++)
+    // {
+    //     for(c=0;c<3;c++)
+    //     {
+    //         printf("%d ",arr3[r][c]);
+    //     }
+    //     printf("\n");
+    // }
+    f3();
     return 0;
 }
