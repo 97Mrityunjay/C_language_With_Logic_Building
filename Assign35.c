1fa6d0c28641aec76914bd3469b20b26d69ea3cb
unknown <mritunjayku99@gmail.com>
2025-06-24 14:21:40 +0530

    Assign35.c file is modified

--- Assign35.c
+++ Assign35.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 #include<string.h>
+void convertLowerCase();
+void reverseString();
+void f3();
+void copyString();
+void firstOccurrenceOfCharacter();
 /*Q1. Write a program to convert a given string into lowercase*/
-void convertLowerCase(char s[])
+void convertLowerCase()
 {
     int i;
+    char s[30];
+    printf("Enter a string\n");
+    fgets(s,30,stdin);
+    for(i=0;s[i]!='\0';i++);
+    s[i-1]='\0';
     for(i=0;s[i];i++)
     {
         if(s[i]>='A'&&s[i]<='Z')
@@ -11,11 +21,14 @@ void convertLowerCase(char s[])
             s[i]=s[i]+32;
         }
     }
+    printf("After converting string into lowercase, string is\n");
+    printf("%s",s);
 }
 /*Q2.Write a program to reverse a string*/
-void reverseString(char s[])
+void reverseString()
 {
-    int l,i,temp;
+    char s[30],temp;
+    int l,i;
     for(l=0;s[l];l++);
     s[l-1]='\0';
     l--;
@@ -25,20 +38,80 @@ void reverseString(char s[])
         s[i]=s[l-i-1];
         s[l-i-1]=temp;
     }
+    printf("Reversed String=%s",s);
+}
+/*Q3.Write a program in C to count the total number of alphabets,digits and special characters in a string*/
+void f3()
+{
+    int i,countAlphabets=0,countDigits=0,countSpecialChar=0;
+    char s[30];
+    printf("Enter String\n");
+    fgets(s,30,stdin);
+    for(i=0;s[i];i++);
+    s[i-1]='\0';
+    for(i=0;s[i];i++)
+    {
+        if(s[i]>='A'&&s[i]<='Z'||s[i]>='a'&&s[i]<='z')
+        {
+            countAlphabets++;
+        }
+        else if(s[i]>='0'&&s[i]<='9')
+        {
+            countDigits++;
+        }
+        else
+        {
+            countSpecialChar++;
+        }
+    }
+    printf("Number of Alphabets in string=%d\n",countAlphabets);
+    printf("Number of Digits in string=%d\n",countDigits);
+    printf("Number of Special Character in string=%d\n",countSpecialChar);
+}
+/*Q4.Write a program in c to copy one string to another char array*/
+void copyString()
+{
+    char str[50];
+    char s[50];
+    int i;
+    printf("Enter string\n");
+    fgets(s,50,stdin);
+    for(i=0;str[i];i++);
+    str[i-1]='\0';
+    for(i=0;str[i]!='\0';i++)
+    {
+        s[i]=str[i];
+    }
+   s[i]='\0';
+   for(i=0;s[i];i++)
+   {
+      printf("%c",s[i]);
+   }
+}
+/*Q5.Write a program to find first occurrence of a given character in given string*/
+void firstOccurrenceOfCharacter()
+{
+    int i,l;
+    char s[30],ch;
+    printf("Enter string\n");
+    fgets(s,30,stdin);
+    printf("Enter a character\n");
+    scanf("%c",&ch);
+    for(l=0;s[l];l++);
+    s[l-1]='\0';
+    l--;
+    for(i=0;s[i];i++)
+    {
+        if(s[i]==ch)
+         break;
+    }
+    if(s[i]!='\0')
+     printf("Character is found first time in given string at index %d",i);
+    else
+    printf("Character is not found in given string");
 }
 int main()
 {
-    // char str[30];
-    // printf("Enter a string\n");
-    // fgets(str,30,stdin);
-    // convertLowerCase(str);
-    // printf("%s",str);
-
-    char str[30];
-    printf("Enter a string\n");
-    // fflush(stdin);
-    fgets(str,30,stdin);
-    reverseString(str);
-    printf("%s",str);
+    f3();
     return 0;
-}
+ }
